b9fd89ae268f12186835d62fe2cdd2b4052db605
MaHongYan22 <78285553+MaHongYan22@users.noreply.github.com>
2023-04-07 10:05:21 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,21 +22,21 @@ const subscribeAliceAndBob = async (api:ApiPromise) => {
   })
 }
 const subscribeEvents = async (api:ApiPromise) => {
-  // Subscribe to system events via storage
+
 api.query.system.events((events: any[]) => {
   console.log(`\nReceived ${events.length} events:`);
 
-  // Loop through the Vec<EventRecord>
+
   events.forEach((record) => {
-    // Extract the phase, event and the event types
+    
     const { event, phase } = record;
     const types = event.typeDef;
 
-    // Show what we are busy with
+   
     console.log(`\t${event.section}:${event.method}:: (phase=${phase})`);
     console.log(`\t\t${event.meta.documentation}`);
 
-    // Loop through each of the parameters, displaying the type and data
+   
     event.data.forEach((data: { toString: () => any; }, index: string | number) => {
       console.log(`\t\t\t${types[index].type}: ${data.toString()}`);
     });
